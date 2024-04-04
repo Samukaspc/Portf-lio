1f243079efd225ae35f563904c3824cbafd4b197
samuka <samukasp003@gmail.com>
2024-04-03 22:34:32 -0300

    correção de paginação

--- src/componete/cardComponente/index.js
+++ src/componete/cardComponente/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Card, Tooltip, Pagination } from 'antd';
+import './style.css';
 
 export default function CardComponente({ dadosProjeto, deletarProjetoCard }) {
     const [currentPage, setCurrentPage] = useState(1);
@@ -7,8 +8,10 @@ export default function CardComponente({ dadosProjeto, deletarProjetoCard }) {
 
     useEffect(() => {
         const totalPages = Math.ceil(dadosProjeto.length / pageSize);
-        if (currentPage > totalPages) {
+        if (currentPage > totalPages && totalPages > 0) {
             setCurrentPage(totalPages);
+        } else if (currentPage === 0 && totalPages > 0) {
+            setCurrentPage(1);
         }
     }, [dadosProjeto, currentPage, pageSize]);
 
@@ -41,15 +44,17 @@ export default function CardComponente({ dadosProjeto, deletarProjetoCard }) {
 
     return (
         <div>
-            <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px' }}>
+            <div  style={{ display: 'flex', flexWrap: 'wrap', gap: '10px' }}>
                 {currentProjects.map((projeto, index) => (
                     <div key={index} style={{ position: 'relative', width: 155, height: 225 }}>
                         <a href={projeto.urlProjeto} target="_blank" rel="noopener noreferrer">
-                            <Card hoverable style={{ width: 135, height: 200 }} cover={<img alt={projeto.nomeProjeto} src={projeto.urlImagem} />}>
+                            <div className='container-card-componete'>
+                            <Card hoverable  cover={<img alt='../../img/embreve.jpg' src={projeto.urlImagem} />}>
                                 <Tooltip title={projeto.nomeProjeto} placement="top" color="blue">
-                                    <Card.Meta title={projeto.nomeProjeto} style={{ display: 'flex', textAlign: 'center' }} />
+                                    <Card.Meta title={projeto.nomeProjeto}  style={{ display: 'flex', textAlign: 'center' }} />
                                 </Tooltip>
                             </Card>
+                            </div>
                         </a>
                         <div style={{ position: 'absolute', top: 0 }}>
                             <Button onClick={() => handleDelete(projeto.nomeProjeto)} type="primary" danger style={{ width: '100%' }}>Deletar</Button>
@@ -58,13 +63,14 @@ export default function CardComponente({ dadosProjeto, deletarProjetoCard }) {
                 ))}
             </div>
             {dadosProjeto.length > pageSize && (
-                    <Pagination
-                        current={currentPage}
-                        total={dadosProjeto.length}
-                        pageSize={pageSize}
-                        onChange={handlePageChange}
-                        itemRender={customItemRender}
-                    />
+                <Pagination
+                    current={currentPage}
+                    total={dadosProjeto.length}
+                    pageSize={pageSize}
+                    onChange={handlePageChange}
+                    itemRender={customItemRender}
+                    style={{ marginTop: '10px' }}
+                />
             )}
         </div>
     );
